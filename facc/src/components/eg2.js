892b600214db15831348329b6945ea507c974a4b
Mair Swartz <mair.swartz@essentialenergy.com.au>
2018-05-21 18:33:05 +1000

    completed facc

--- facc/src/components/eg2.js
+++ facc/src/components/eg2.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 import { Position } from './facc';
 export class Eg2 extends Component {
-  state = {
-    position: { x: 0, y: 0 }
-  };
-
-  registerMouseEvent = element =>
-    (element.onmousemove = e => this.setState({ position: { x: e.clientX, y: e.clientY } }));
-
   render() {
-    const { x, y } = this.state.position;
     return (
       <Position>
         {pos => (
-          <div
-            ref={this.registerMouseEvent}
-            style={{ backgroundColor: `hsl(0, ${pos.x % 100}%, ${pos.y % 100}% )` }}
-          >
+          <div style={{ backgroundColor: `hsl(0, ${pos.x % 100}%, ${pos.y % 100}% )` }}>
             <div>{`X is ${pos.x} and Y is ${pos.y}`}</div>
           </div>
         )}
